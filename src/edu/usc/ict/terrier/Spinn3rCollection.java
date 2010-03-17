e149938227a79f88dc9d8a0aba264b93e4600c6e
Ruey-Cheng <rueycheng@gmail.com>
2010-03-17 01:35:15 -0700

    Just figured out a clever way to do this.

--- src/edu/usc/ict/terrier/Spinn3rCollection.java
+++ src/edu/usc/ict/terrier/Spinn3rCollection.java
@@ -2,20 +2,30 @@ package edu.usc.ict.terrier;
 
 import org.terrier.indexing.Collection;
 import org.terrier.indexing.Document;
+import java.io.*;
 import java.util.*;
+import java.util.zip.GZIPInputStream;
 
 public class Spinn3rCollection implements Collection {
-    private ListIterator<String> listOfFiles;
-    private String openedFile;
+    private InputStream in;
+    private boolean endOfStream = false;
 
     public Spinn3rCollection(String[] filenames) { 
-	listOfFiles = Arrays.asList(filenames).listIterator();
-	openedFile = null;
+	Vector<InputStream> streams = new Vector<InputStream>();
+	for (String filename: filenames) {
+	    try {
+		streams.add(filename.endsWith(".gz")? 
+		    new GZIPInputStream(new FileInputStream(filename)): new FileInputStream(filename));
+	    }
+	    catch (FileNotFoundException e) {}
+	    catch (IOException e) { e.printStackTrace(); }
+	}
+
+	in = new SequenceInputStream(streams.elements());
+	nextDocument();
     }
 
-    public boolean endOfCollection() { 
-	return openedFile == null && !listOfFiles.hasNext();
-    }
+    public boolean endOfCollection() { return endOfStream; }
 
     public Document getDocument() { 
 	return new Spinn3rDocument(new java.io.StringReader("Hey you"), new HashMap<String, String>()); 
@@ -25,7 +35,7 @@ public class Spinn3rCollection implements Collection {
 	return false; 
     }
 
-    public void reset() {}
+    public void reset() { try { in.reset(); } catch (IOException e) { e.printStackTrace(); } }
     public void close() {}
 
     // Arrrgh.  Why should I implement a deprecated method?
